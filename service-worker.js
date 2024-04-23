07d8abd38f284b7f7bd60b608e9e6975f4092859
dicosrio <153856431+dicosrio@users.noreply.github.com>
2024-04-23 19:19:14 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/details.html',
+    '/styles/main.css',
+    '/scripts/main.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
